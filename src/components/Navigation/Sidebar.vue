92cf60bdce83f1aee66c9d58e7d31dee898cf149
ranonsew <ranonsew@gmail.com>
2021-08-16 14:55:11 +0800

    trying to add cool transition effect
    need more time to decide and figure out the css required to get everything in place and where I want it to all go
    
    big problems for now include the sidebar tooltips over the icons when collapsed
    also include the logo text or something appearing on the side when sidebar is uncollapsed (it appears suddenly and flips with the icon, I want it to slide out from the left instead with a fade in transition)
    and also include header bar and also the search functionality which would require some hardships later on.

--- src/components/Navigation/Sidebar.vue
+++ src/components/Navigation/Sidebar.vue
@@ -1,27 +1,49 @@
 <template>
   <div id="sidebar" :style="{ width: sidebarWidth }">
-    <h1>
-      <span v-if="collapsed">
-        <div>V</div>
-        <!-- <div>JS</div> -->
-      </span>
-      <span v-else>Vue</span>
-    </h1>
-
-    <!-- tool tip on icon? Or in sidebarlinks? -->
-    <SidebarLinks to="/" icon="fas fa-home" title="Home">Home</SidebarLinks>
-    <SidebarLinks to="/about" icon="fas fa-image" title="About">About</SidebarLinks>
-    <SidebarLinks to="/friends" icon="fas fa-users" title="Friends">Friends</SidebarLinks>
-    <SidebarLinks to="/dashboard" icon="fas fa-columns" title="Dashboard">Dashboard</SidebarLinks>
-
     <!--double-left does a rotate-180 when collapsed is true-->
-    <span
+    <!-- <span
       class="collapse-icon"
       :class="{ 'rotate-180': collapsed }"
       @click="toggleSidebar"
     >
       <i class="fas fa-angle-double-left" />
-    </span>
+    </span> -->
+    <h2>
+      <!-- <span class="line-1"></span> -->
+      <span
+        class="collapse-icon"
+        :class="{ 'rotate-180': collapsed }"
+        @click="toggleSidebar"
+      >
+        <i class="fas fa-angle-double-left" />
+        <span v-if="collapsed">
+          <span></span>
+        </span>
+        <span class="spacing-1" v-else>Vue</span>
+      </span>
+      <!-- <span class="line-1"></span> -->
+    </h2>
+
+    <!-- tool tip on icon? Or in sidebarlinks? -->
+    <SidebarLinks to="/" icon="fas fa-home" title="Home">Home</SidebarLinks>
+    <SidebarLinks to="/about" icon="fas fa-image" title="About"
+      >About</SidebarLinks
+    >
+    <SidebarLinks to="/friends" icon="fas fa-users" title="Friends"
+      >Friends</SidebarLinks
+    >
+    <SidebarLinks to="/dashboard" icon="fas fa-columns" title="Dashboard"
+      >Dashboard</SidebarLinks
+    >
+
+    <!--double-left does a rotate-180 when collapsed is true-->
+    <!-- <span
+      class="collapse-icon"
+      :class="{ 'rotate-180': collapsed }"
+      @click="toggleSidebar"
+    >
+      <i class="fas fa-angle-double-left" />
+    </span> -->
   </div>
 </template>
 
@@ -38,7 +60,8 @@ export default {
 };
 </script>
 
-<style> /* this here style isn't scoped, so that's why lmao */
+<style>
+/* this here style isn't scoped, so that's why lmao */
 :root {
   /* --sidebar-bg-color: #2f4a85;
   --sidebar-item-hover: #384ba1;
@@ -66,17 +89,17 @@ export default {
   display: flex;
   flex-direction: column;
 }
-#sidebar h1 {
+#sidebar h2 {
   height: 2.5em;
 }
-#sidebar h1::before {
+.line-1::before {
   content: "";
   background-color: white;
   display: block;
   height: 0.3rem;
   width: 100%;
 }
-#sidebar h1::after {
+.line-1::after {
   content: "";
   background-color: white;
   display: block;
@@ -85,8 +108,9 @@ export default {
 }
 .collapse-icon {
   position: absolute;
-  bottom: 0;
-  padding: 0.75em;
+  /* top: 0; */
+  left: 0;
+  padding: 0 0.75em;
   color: rgba(255, 255, 255, 0.85);
   transition: 0.2s linear;
   cursor: pointer;
@@ -94,5 +118,9 @@ export default {
 .rotate-180 {
   transform: rotate(180deg);
   transition: 0.2s linear;
+}
+.spacing-1 {
+  padding-left: 0.5em;
+  transition: 1s ease;
 }
 </style>
