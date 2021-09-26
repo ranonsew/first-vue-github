04f215087a7c52b81f1d033a8c6f55cdf35e01c9
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2021-09-27 03:06:27 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import router from "./router";
 import store from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import "@fortawesome/fontawesome-free/js/all";
 import axios from "axios";
 
 Vue.prototype.$http = axios; // for some reason can't use axios on its own?
 
-import "@fortawesome/fontawesome-free/js/all";
 
 createApp(App).use(store).use(router).mount("#app");
