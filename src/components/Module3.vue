09bce286d5222f96c9825bf941baa9646e067154
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2021-10-11 11:27:38 +0800

    Create Module3.vue

---
+++ src/components/Module3.vue
@@ -0,0 +1,29 @@
+<template>
+  <label for="locale">locale</label>
+  <select v-model="$i18n.locale">
+    <option>en</option>
+    <option>cn</option>
+    <option>jp</option>
+  </select>
+  <p>message: {{ $t("welcome") }}</p>
+</template>
+
+<script>
+export default {
+  name: "Module3",
+};
+</script>
+
+<i18n>
+{
+  "en": {
+    "welcome": "Hello Everyone!"
+  },
+  "jp": {
+    "welcome": "こんにちは、みんな!"
+  },
+  "cn": {
+    "welcome": "大家好!"
+  }
+}
+</i18n>
